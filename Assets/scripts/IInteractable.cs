580f31186f0b6416f7c8078b194278d1df045904
Sam Salfi <samsalfi@yahoo.ca>
2024-09-08 16:02:39 -0300

    Updated interactable interface

--- Assets/scripts/IInteractable.cs
+++ Assets/scripts/IInteractable.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using Interact;
 
 public interface IInteractable
 {
-    public void Interact(GameObject interactor);
+    public void Interact(Interactor interactor);
+    public bool CanInteract(Interactor interactor);
 }
