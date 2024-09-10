5e4e0da83a0f122b005554b6f60781d4c69882de
WHHWII <36775534+WHHWII@users.noreply.github.com>
2024-09-10 01:16:06 -0500

    Radio

---
+++ Assets/scripts/GrabbyRadioInteractable.cs
@@ -0,0 +1,20 @@
+using Grabbing;
+using Interact;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbyRadioInteractable : Grabbable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/RadioInteractable.cs
@@ -0,0 +1,28 @@
+using Grabbing;
+using Interact;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioInteractable : TriggerInteractor
+{
+    [SerializeField] AudioSource player;
+    [SerializeField] List<AudioClip> trackList;
+    int currentTrack = 0;
+
+    public override void Interact(GameObject gameObject)
+    {
+        base.Interact(gameObject);
+        player.Stop();
+        if(currentTrack == trackList.Count-1)
+        {
+            currentTrack = 0;
+        }
+        else
+        {
+            ++currentTrack;
+        }
+        player.clip = trackList[currentTrack];
+        player.Play();
+    }
+}
