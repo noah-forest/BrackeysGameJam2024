0ae3c9a76487062f58c86c8237e318c08986a176
ZTattempt <62923868+noah-forest@users.noreply.github.com>
2024-09-11 01:51:51 -0500

    added anims to player

--- Assets/scripts/Player/PlayerController.cs
+++ Assets/scripts/Player/PlayerController.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private float speed = 12f;
+	private static readonly int speed = Animator.StringToHash("speed");
+	[SerializeField] private float moveSpeed = 8f;
 
 	Vector3 move;
+	private Animator animator;
 
 	private CharacterController characterController;
 	private void Start()
 	{
 		characterController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
 	}
 
 	private void Update()
 	{
 		float x = Input.GetAxisRaw("Horizontal");
 		float z = Input.GetAxisRaw("Vertical");
-
+		
 		move = transform.right * x + transform.forward * z;
-		characterController.Move(move.normalized * (speed * Time.deltaTime) + Physics.gravity * Time.deltaTime);
+		characterController.Move(move.normalized * (moveSpeed * Time.deltaTime) + Physics.gravity * Time.deltaTime);
+
+		//set up anims
+		if (move == Vector3.zero)
+		{
+			//idle
+			animator.SetFloat(speed, 0);
+		}
+		else
+		{
+			//walking
+			animator.SetFloat(speed, 0.5f);
+		}
 	}
 }
