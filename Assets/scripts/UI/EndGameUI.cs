e6c2cbce4a5f606338defcb9113b571fdf38305f
WHHWII <36775534+WHHWII@users.noreply.github.com>
2024-09-13 22:18:18 -0500

    Squimble

--- Assets/scripts/UI/EndGameUI.cs
+++ Assets/scripts/UI/EndGameUI.cs
@@ -8,6 +8,17 @@ public class EndGameUI : MonoBehaviour
 {
 	public GameObject wonScreen;
 	public GameObject lostScreen;
+
+	public GameObject wonText;
+	public GameObject postWinText;
+    public GameObject lostText;
+    public GameObject postLossText;
+	public GameObject buttons;
+
+	[SerializeField]AudioClip[] sounds;
+	public AudioSource TextChangeSFX;
+
+	public float postScreenDelay = 1.5f;
 	
 	private GameManager gameManager;
 
@@ -15,7 +26,10 @@ public class EndGameUI : MonoBehaviour
 	{
 		wonScreen.SetActive(false);
 		lostScreen.SetActive(false);
-		
+		postLossText.SetActive(false);
+		postWinText.SetActive(false);
+		buttons.SetActive(false);
+
 		gameManager = GameManager.singleton;
 
 		Cursor.lockState = CursorLockMode.Confined;
@@ -30,10 +44,41 @@ public class EndGameUI : MonoBehaviour
 			// lost
 			lostScreen.SetActive(true);
 		}
+		StartCoroutine(ShowPostScreen());
+	}
+
+	IEnumerator ShowPostScreen()
+	{
+		yield return new WaitForSeconds(postScreenDelay);
+        if (gameManager.gameState == GameManager.GameState.victory)
+        {
+			// won
+			TextChangeSFX.clip = sounds[0];
+			TextChangeSFX.Play();
+			wonText.SetActive(false);
+            postWinText.SetActive(true);
+        }
+        else
+        {
+            // lost
+            TextChangeSFX.clip = sounds[1];
+            TextChangeSFX.Play();
+            lostText.SetActive(false);
+            postLossText.SetActive(true);
+        }
+		StartCoroutine(ShowButtons());
+    }
+
+	IEnumerator ShowButtons()
+	{
+		yield return new WaitForSeconds(postScreenDelay/2);
+        TextChangeSFX.clip = sounds[2];
+        buttons.SetActive(true);
 	}
 
 	public void BackToMainMenu()
 	{
 		gameManager.LoadMenuScene();
 	}
+
 }
